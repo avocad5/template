8bbe4c650fce4425ac702805920a5973896ded5b
avocad5 <63831073+avocad5@users.noreply.github.com>
2020-08-30 17:28:48 +0900

    Update app.js

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ const career = '2년' //여기에 자신의 경력을 입력하세요
 const explanation = '코딩과 수학을 좋아하는 평법한 중1' //여기에 자기소개를 입력하세요
 const maincolor = '#87FFB7' //여기에 메인컬러를 입력하세요
 const subcolor = '#BFFFC3' //여기에 서브컬러를 
-
-
-
 const projectname = ['3d ball run','알까기의왕','엔플러스엔진']; //여기에 자기 엔트리작품 이름을 입력하세요
-
 const projectment = ['렉 안걸리는 3d!','인공지능과 함께 알까기를 즐겨보세요!','야, 너두 3d만들수있어'] //여기에 자기 엔트리작품 소개를 입력하세요
-
 const projectid = ['5e3f6015960df10024ab07a7','5e00a9134a413900566992bb','5e95ba64614b6000ec78f37f'] //여기에 자기 엔트리작품 id를 입력하세요
+
+
+
+
+
 
 for(var i = 0; i<projectname.length; i++){ //버튼 생성
   document.getElementsByClassName('project')[0].innerHTML += `<a href="https://playentry.org/${name}/${projectid[i]}"><img src="https://playentry.org/uploads/thumb/${projectid[i].substring(0,4)}/${projectid[i]}.png"><div><p class="pname"><strong>${projectname[i]}</strong></p><p class="pment">${projectment[i]}</p></div></a>`
