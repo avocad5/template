797487dff389201cd465b9821d27ac968cef9837
mango-friend <70262504+mango-friend@users.noreply.github.com>
2020-08-30 17:53:01 +0900

    Update app.js
    ㅇ

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const name = 'avocad5' //여기에 자기 엔트리아이디를 입력하세요
-const userid = '5d773c885b1f435931998d8e' //여기에 자기 유저아이디를 입력
-const gitname = 'avocad5' //여기에 자기 깃허브아이디를 입력하세요
-const nickname = '아보카도' //여기에 자기 별명을 입력하세요
-const myment = '더 참신하게, 더 재미있게, 더 멋지게' //여기에 상태메시지를 
-const mywork = '알까기의왕' //여기에 자신의 대표작품을 입력하세요
+const name = 'mangofriend' //여기에 자기 엔트리아이디를 입력하세요
+const userid = '5efd4fe435de2c0037665842' //여기에 자기 유저아이디를 입력
+const gitname = 'mangofriend' //여기에 자기 깃허브아이디를 입력하세요
+const nickname = '망고프렌드' //여기에 자기 별명을 입력하세요
+const myment = '달달한 망고주스와 행복한 하루' //여기에 상태메시지를 
+const mywork = '망고챗' //여기에 자신의 대표작품을 입력하세요
 const career = '2년' //여기에 자신의 경력을 입력하세요
-const explanation = '코딩과 수학을 좋아하는 평법한 중1' //여기에 자기소개를 입력하세요
-const maincolor = '#87FFB7' //여기에 메인컬러를 입력하세요
-const subcolor = '#BFFFC3' //여기에 서브컬러를 
-const projectname = ['3d ball run','알까기의왕','엔플러스엔진']; //여기에 자기 엔트리작품 이름을 입력하세요
-const projectment = ['렉 안걸리는 3d!','인공지능과 함께 알까기를 즐겨보세요!','야, 너두 3d만들수있어'] //여기에 자기 엔트리작품 소개를 입력하세요
-const projectid = ['5e3f6015960df10024ab07a7','5e00a9134a413900566992bb','5e95ba64614b6000ec78f37f'] //여기에 자기 엔트리작품 id를 입력하세요
+const explanation = '갓나온 신선한 망고' //여기에 자기소개를 입력하세요
+const maincolor = '#FFCE00' //여기에 메인컬러를 입력하세요
+const subcolor = '#FFA300' //여기에 서브컬러를 
+const projectname = ['망고챗','망고캡챠','합이 7 핵심코드']; //여기에 자기 엔트리작품 이름을 입력하세요
+const projectment = ['소규모 모임부터 대규모 회의까지','너 솔직히 사람 아니잖어','합이 7의 핵심 코드'] //여기에 자기 엔트리작품 소개를 입력하세요
+const projectid = ['5f0e886c087c9d01032a1090','5f3cc9b471ec0e021c82288f','5f328a517425a6009e40edd1'] //여기에 자기 엔트리작품 id를 입력하세요
 
 
 
