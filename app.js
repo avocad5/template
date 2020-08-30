1c87032890c78c10cd564e891cc7649952caa7e4
avocad5 <63831073+avocad5@users.noreply.github.com>
2020-08-30 15:54:32 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const name = 'avocad5' //여기에 자기 엔트리아이디를 입력하세요
-const ninkname = '아보카도' //여기에 자기 별명을 입력하세요
+const nickname = '아보카도' //여기에 자기 별명을 입력하세요
 const explanation = '코딩과 수학을 좋아하는 평법한 중1' //여기에 자기소개를 입력하세요
 
 const projectname = ['3d ball run','알까기의왕','엔플러스엔진']; //여기에 자기 엔트리작품 이름을 입력하세요
@@ -11,3 +11,4 @@ const projectid = ['5e3f6015960df10024ab07a7','5e00a9134a413900566992bb','5e95ba
 for(var i = 0; i<projectname.length; i++){ //버튼 생성
   document.getElementsByClassName('project')[0].innerHTML += `<a href="https://playentry.org/${name}/${projectid[i]}"><img src="https://playentry.org/uploads/thumb/${projectid[i].substring(0,5)}/${projectid[i]}.png"><div><p class="pname"><strong>${projectname[i]}</strong></p><p class="pment">${projectment[i]}</p></div></a>`
 }
+document.getElementByClassName('about')[0].innerHTML = `<p>이름 : ${name}</p><p>별명 : ${nickname}</p><p>소개 : ${explanation}</p>
